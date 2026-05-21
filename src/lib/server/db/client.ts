ec8626d6d4c69b26a893c2d0b32b77bacc09ba85
Zyxary <kelpselp@gmail.com>
2026-05-21 23:53:17 +0800

    Using workers as db client

--- src/lib/server/db/client.ts
+++ src/lib/server/db/client.ts
@@ -2,7 +2,7 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const databaseUrl = process.env.DATABASE_URL;
+const databaseUrl = "https://grocify-api.kelpselp.workers.dev";
 
 if (!databaseUrl) {
   throw new Error("DATABASE_URL is required for API routes.");
